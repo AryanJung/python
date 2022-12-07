9730fec6cd1d4c2789ee1f9fc42ee2f24e668140
AryanJung <119690880+AryanJung@users.noreply.github.com>
2022-12-07 09:55:07 +0545

    Add files via upload

---
+++ erthquak.py
@@ -0,0 +1,35 @@
+import csv
+import os
+
+filename = 'earthquakePast.csv'
+
+lats,lons = [], []
+epicentre,magnitudes = [],[]
+timestrings = []
+
+l=[]
+#with statement automatically closes the file again when the block end
+#next(reader) # Move to next Row, Ignore the header in this case.
+'''
+    fp=open('','r')
+    ......
+    ......
+    fp.close()
+'''
+
+#'c:\python37\test.txt'
+
+#with open(os.path.dirname(os.path.abspath(__file__))+'/'+filename) as f:
+l=[]
+with open(filename) as f:  #file handler
+    reader = csv.reader(f)
+    #print(reader)
+    #print(type(reader))
+    for row in reader:
+        print(row[2])
+        l.append(row[2])
+        print(" ",l)
+len(l)
+max(l)
+min(l)
+
